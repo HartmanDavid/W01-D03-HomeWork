cee65abf6be81bf1f96eca458f735f729ad1912e
David Hartman <david@petminded.com>
2015-06-24 23:28:04 -0700

    JS loop completed - 90percent done

--- javascript.js
+++ javascript.js
@@ -1,15 +1,20 @@
 var wdi_17_names = ["Addison", "Ben", "Christine", "Chloe", "Cris", "Dane", "David H", "David Z", "Emily", "Imtaek", "Jack", "Jesse P", "Jesse Z", "Jonathan", "Pericles", "Peter", "Philippe", "Sergio", "Simon", "Tali", "Tavo", "Trevor"];
+var used_names_array = [];
+var i = 0; //used to count the for loop
+var counter = 0; //used to rest the count of the eachName function
+    function eachName(){
 
-var i = 0;
- function eachName(){
-
-   for (i = 0; i < wdi_17_names.length; i++) {
-      //REFACTORED
-        //Math.floor and Math.random might be useful
-    var pick = Math.floor(Math.random() * wdi_17_names.length)
-        if (used_names_array.indexof(push) === -1){
-        used_names_array.push(pick);
-
-
-var namePicker = wdi_17_names[pick];
-console.log(namePicker);
+      for (i = 0; i < wdi_17_names.length; i++) {
+            var pick = Math.floor(Math.random() * wdi_17_names.length);
+            if ((used_names_array.indexOf(pick)) === -1){
+                used_names_array.push(pick);
+                var namePicker = wdi_17_names[pick];
+                console.log(namePicker);
+            }else if(used_names_array.length < wdi_17_names.length){
+                eachName();
+            }
+      // the loop logs each string in the array
+      //notice the difference in what is logged
+        }
+    }
+ eachName();
